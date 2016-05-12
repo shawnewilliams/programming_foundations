54e71e4288b7da442dfe307b66021192b4100f92
Shawn Williams <shawn79williams@yahoo.com>
2016-05-12 14:42:08 -0400

    Programming Foundations

--- lesson_2/loan_calc.rb
+++ lesson_2/loan_calc.rb
@@ -5,27 +5,32 @@ def prompt(message)
   puts"=>#{message}"
 end
 
-def monthly_rate(interest)
-  (interest / 12) / 100
-end
-
-def loan_months(yrs)
-  yrs * 12
-end
-
-def valid_num(num)
+def valid_num?(num)
   /\d/.match(num) && /^\d*\.?\d*$/.match(num)
 end
 
-def monthly_pmt(total, apr, years)
-  c = monthly_rate(apr.to_f)
-  n = loan_months(years.to_f)
+def monthly_rate(rate)
+  (rate.to_f / 12) / 100
+end
+
+def years_to_months(yrs)
+  yrs.to_f * 12
+end
+
+def print_monthly_pmt(total, interest, yrs)
+  c = monthly_rate(interest)
+  n = years_to_months(yrs)
   payments = total.to_f * ((c * (1 + c)**n) / ((1 + c)**n - 1))
   format("$%.2f", payments)
 end
 
-def y_or_n(answer)
-  answer.downcase == 'y' || answer.downcase == 'n'
+def print_no_interest_pmt(total, yrs)
+  payments = total.to_f / years_to_months(yrs)
+  format("$%.2f", payments)
+end
+
+def y_or_n?(answer)
+  answer.casecmp('y') == 0 || answer.casecmp('n') == 0
 end
 
 # Start program
@@ -39,7 +44,7 @@ loop do # main loop
 
   loop do
     amount = gets.chomp
-    if valid_num(amount) && amount.to_f > 0
+    if valid_num?(amount) && amount.to_f > 0
       break
     else
       prompt "Hmm... That doesn't look like a valid number. Please try again."
@@ -51,7 +56,7 @@ loop do # main loop
   prompt "(Example 5 = 5% and 5.5 = 5.5%)"
   loop do
     apr = gets.chomp
-    if valid_num(apr) && apr.to_f > 0
+    if valid_num?(apr) && apr.to_f >= 0
       break
     else
       prompt "Hmm... That doesn't look like a valid number. Please try again."
@@ -62,28 +67,32 @@ loop do # main loop
 
   loop do
     years = gets.chomp
-    if valid_num(years) && years.to_f > 0
+    if valid_num?(years) && years.to_f > 0
       break
     else
       prompt "Hmm... That doesn't look like a valid number. Please try again."
     end
   end
 
-  prompt "Your monthly payment is: #{monthly_pmt(amount, apr, years)}"
+  if apr.to_f == 0
+    prompt "Your monthly payment is: #{print_no_interest_pmt(amount, years)}"
+  else
+    prompt "Your monthly payment is: #{print_monthly_pmt(amount, apr, years)}"
+  end
   puts
 
   continue = ''
   prompt "Would you like to try a different amount? (Y or N)"
   loop do
     continue = gets.chomp
-    if y_or_n(continue)
+    if y_or_n?(continue)
       break
     else
       prompt "Hmm... That doesn't look like a valid answer. Pleas enter Y or N."
     end
   end
 
-  break unless continue.downcase.start_with?('y')
+  break unless continue.casecmp('y') == 0
 end
 
 prompt "Good Bye!"
